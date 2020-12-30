803f3c612eaacc8d2ea5167bf5949cd96d0ca106
SMRuggiero <stevemruggiero@gmail.com>
2020-12-30 15:21:42 -0800

    Placeholder for work on abilities.

---
+++ fightSim/abilityClasses.js
@@ -0,0 +1,11 @@
+
+//Start by implementing Guts
+//Event emitted by CalcDamageMuli?
+//Nah, can use a similar system to manage other multipliers
+export class Ability{
+
+    constructor(){
+
+    }
+
+};
