0429d99cc596876ea612824c6714cd2a9de92e6f
May Roussel <may@maybreak.com>
2021-06-29 17:57:28 -0500

    Fix some exp route floor ordering.

--- pages/experience/index.tsx
+++ pages/experience/index.tsx
@@ -265,7 +265,7 @@ const ExperienceRoute: NextPage = () => {
               <option value={3}>3 (Ruby/Sapphire/Emerald)</option>
               <option value={4}>4 (Diamond/Pearl/Platinum)</option>
               <option value={5}>5 (Black/White and Black 2/White 2)</option>
-              <option value={6}>6+ (X/Y)</option>
+              <option value={6}>6 (X/Y)</option>
               <option value={7}>7+ (Sun/Moon, Ultra Sun/Ultra Moon, and Sword/Shield)</option>
               <option value="lgpe">Let&apos;s Go</option>
             </select>
