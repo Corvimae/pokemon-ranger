41080f066977bfee779730496512c1923bc37b4e
May Roussel <may@maybreak.com>
2021-07-16 13:57:48 -0500

    alias?

--- directives/directiveConverter.ts
+++ directives/directiveConverter.ts
@@ -1,30 +1,29 @@
 /* eslint-disable no-param-reassign */
 // eslint-disable-next-line import/no-unresolved
-import { Node } from 'unist';
+import { Node as UNode } from 'unist';
 import { Transformer } from 'unified/types/ts3.4/index';
 import h from 'hastscript';
 import visit from 'unist-util-visit';
 
-function childrenToText(node: Node): string {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  if ((node as any).value) return (node as any).value as string ?? '';
+function childrenToText(node: UNode): string {
+  if (node.value) return node.value as string ?? '';
 
-  return (node?.children as Node[])?.map(childrenToText).join('\n') ?? '';
+  return (node?.children as UNode[])?.map(childrenToText).join('\n') ?? '';
 }
 
 export function directiveConverter(): Transformer {
   return transform;
 
-  function transform(tree: Node): void {
+  function transform(tree: UNode): void {
     visit(tree, ['textDirective', 'leafDirective', 'containerDirective'], onDirective);
   }
 
-  function onDirective(node: Node) {
+  function onDirective(node: UNode) {
     const data = node.data || (node.data = {});
-    const hast = h(node.name as string, node.attributes as string | Node | (string | Node)[]);
+    const hast = h(node.name as string, node.attributes as string | UNode | (string | UNode)[]);
 
     if (node.type === 'containerDirective') {
-      node.children = ((node.children as Node[]) || []).map(child => {
+      node.children = ((node.children as UNode[]) || []).map(child => {
         if (child.data?.directiveLabel) {
           child.type = 'paragraph';
           child.data.hName = 'containerLabel';
